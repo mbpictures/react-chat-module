c798b77cdbd5138eb1494ae8b23ff2f0ebcd4660
Marius Butz <mbpictures13@gmail.com>
2021-03-28 00:01:23 +0100

    add text message to video message

--- src/Message/MessageVideo.tsx
+++ src/Message/MessageVideo.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { Message } from "./Message";
+import { MessageFactory } from "./MessageFactory";
 
 interface Props {
     message: Message;
 }
 
 export function MessageVideo(props: Props): JSX.Element {
+    const textMessage: Message = {
+        createdAt: props.message.createdAt,
+        type: "text",
+        text: props.message.text,
+        senderId: props.message.senderId,
+        messageId: props.message.messageId,
+        profilePicture: props.message.profilePicture,
+    };
     return (
         <div>
-            <video src={props.message.photo} />
+            <video src={props.message.video} />
+            {MessageFactory.makeInnerMessage(textMessage)}
         </div>
     );
 }
