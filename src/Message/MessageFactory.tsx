3a2418b6cec96ea9ddec5db58928d50192bbb9d5
Marius Butz <mbpictures13@gmail.com>
2021-03-27 23:36:36 +0100

    Update MessageFactory.tsx

--- src/Message/MessageFactory.tsx
+++ src/Message/MessageFactory.tsx
@@ -8,13 +8,14 @@ import { MessageBubble } from "./MessageBubble";
 import { MessageVideo } from "./MessageVideo";
 
 export class MessageFactory {
-    static makeInnerMessage(message: Message): JSX.Element | undefined {
+    static makeInnerMessage(message: Message): JSX.Element | null {
         if (message.type === "typing") return <MessageTyping />;
-        if (message.type === "text") return <MessageText message={message} />;
+        if (message.type === "text" && message.text)
+            return <MessageText message={message} />;
         if (message.type === "photo") return <MessagePhoto message={message} />;
         if (message.type === "video") return <MessageVideo message={message} />;
         if (message.type === "file") return <MessageFile message={message} />;
-        return undefined;
+        return null;
     }
 
     static makeMessage(message: Message, userId: string): JSX.Element {
