c4048460db75e5fd98e835de75ef3ff9a16eec74
Marius Butz <mbpictures13@gmail.com>
2021-03-27 23:36:46 +0100

    add text to photo message

--- src/Message/MessagePhoto.tsx
+++ src/Message/MessagePhoto.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Message } from "./Message";
+import style from "../style/MessagePhoto.scss";
+import { MessageFactory } from "./MessageFactory";
 
 interface Props {
     message: Message;
 }
 
 export function MessagePhoto(props: Props): JSX.Element {
+    const textMessage = props.message;
+    textMessage.type = "text";
     return (
-        <div>
+        <div className={style.photo}>
             <img src={props.message.photo} />
+            {MessageFactory.makeInnerMessage(textMessage)}
         </div>
     );
 }
