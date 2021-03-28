13e99a3e7ee1b2b0974e86358c3b62496bcb0d14
Marius Butz <mbpictures13@gmail.com>
2021-03-28 16:49:03 +0200

    add incomming message animation based on read flag

--- src/Message/MessageBubble.tsx
+++ src/Message/MessageBubble.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createRef, useEffect, useState } from "react";
 import style from "../style/MessageBubble.scss";
 import { Message } from "./Message";
 
@@ -9,14 +9,33 @@ interface Props {
 }
 
 export function MessageBubble(props: Props) {
+    const containerRef = createRef<HTMLDivElement>();
+    const [messageRead, setMessageRead] = useState<boolean>();
+
     const senderClass =
         props.userId === props.message.senderId
             ? style.from_me
             : style.from_them;
     const classNames = style.message + " " + senderClass;
 
+    useEffect(() => {
+        if (props.message.read === undefined || props.message.read) {
+            setMessageRead(true);
+            return;
+        }
+        setTimeout(() => {
+            setMessageRead(true);
+        }, 100);
+    }, []);
+    let animationClass = messageRead ? style.animated_in : "";
+    animationClass +=
+        props.message.read === undefined ? " " + style.not_animated : "";
+
     return (
-        <div className={`${style.message_container} ${senderClass}`}>
+        <div
+            className={`${style.message_container} ${senderClass} ${animationClass}`}
+            ref={containerRef}
+        >
             <div className={classNames}>{props.children}</div>
             <div className={style.time}>
                 {props.message.createdAt.toLocaleTimeString()}
