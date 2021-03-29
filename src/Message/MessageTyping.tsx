3bd80c32347055e15c50edd7972501d0d0279235
Marius Butz <mbpictures13@gmail.com>
2021-03-29 20:46:00 +0200

    implement typing animation

--- src/Message/MessageTyping.tsx
+++ src/Message/MessageTyping.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import style from "../style/MessageTyping.scss";
 
-export function MessageTyping() {
-    return <div />;
+export function MessageTyping(): JSX.Element {
+    return (
+        <div className={style.typing}>
+            <span />
+            <span />
+            <span />
+        </div>
+    );
 }
