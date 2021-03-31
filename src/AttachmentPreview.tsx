de721377f97e8f972f5aac9f74544a6e0ecf0048
Marius Butz <mbpictures13@gmail.com>
2021-03-31 23:32:04 +0200

    add base for attachment preview

---
+++ src/AttachmentPreview.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import style from "./style/AttachmentPreview.scss";
+import { Message } from "./Message/Message";
+import { IoCloseOutline } from "react-icons/io5";
+
+interface Props {
+    attachment: Message;
+    onCancel: () => any;
+}
+
+export function AttachmentPreview(props: Props) {
+    return (
+        <div className={style.attachment_preview}>
+            <div className={style.header}>
+                <span>Preview</span>
+                <button onClick={props.onCancel}>
+                    <IoCloseOutline />
+                </button>
+            </div>
+            <div>
+
+            </div>
+            {props.attachment.type}
+        </div>
+    );
+}
