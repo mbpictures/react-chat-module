d163709bf17dca507416b3612b00c4665897bec4
Marius Butz <mbpictures13@gmail.com>
2021-04-04 21:02:49 +0200

    replace native audio player with custom one

--- src/Message/FactoryClasses/MessageAudio.tsx
+++ src/Message/FactoryClasses/MessageAudio.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { ChatMessage } from "../Message";
-import style from "../../style/Message/MessageAudio.scss";
+import { AudioPlayer } from "../../Util/AudioPlayer";
 
 interface Props {
     message: ChatMessage;
 }
 
-export function MessageAudio(props: Props): JSX.Element {
-    return <audio className={style.audio} src={props.message.audio} controls />;
+export function MessageAudio(props: Props): JSX.Element | null {
+    if (!props.message.audio) return null;
+    return <AudioPlayer file={props.message.audio} />;
 }
