e8ad42b4a11edc07b4e8307ed078d6a8cac22594
Marius Butz <mbpictures13@gmail.com>
2021-04-12 14:05:01 +0200

    add loading spinner to image message

--- src/Message/FactoryClasses/MessageImage.tsx
+++ src/Message/FactoryClasses/MessageImage.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import style from "../../style/Message/MessagePhoto.scss";
 import { MessageProp } from "../MessageFactory";
+import { ElementLoad } from "../../Util/ElementLoad";
 
-export const MessageImage = (props: MessageProp) => {
-    return <img className={style.photo} src={props.message.photo} />;
+export const MessageImage: FunctionComponent<MessageProp> = (props) => {
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        setLoaded(false);
+    }, [props.message]);
+
+    const onLoad = () => {
+        setLoaded(true);
+    };
+
+    return (
+        <ElementLoad loaded={loaded} loadingSpinner={props.loadingSpinner}>
+            <img
+                className={style.photo}
+                src={props.message.photo}
+                onLoad={onLoad}
+            />
+        </ElementLoad>
+    );
 };
